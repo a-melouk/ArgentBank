7adbd4bec581bbdbfe7b8b7e6709fc4162b49015
a-melouk <abdoumelouk96@gmail.com>
2024-04-05 19:21:12 +0100

    Updated Dockerfile and compose to run app

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const databaseUrl =
-  process.env.DATABASE_URL || "mongodb://127.0.0.1/argentBankDB";
+  process.env.MONGO_URI || "mongodb://localhost:27017/argentBankDB";
 
 module.exports = async () => {
   try {
